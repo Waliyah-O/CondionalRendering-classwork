67318c0297c20c4efd0630a43ab585dfa01efd98
Waliyah Okunade <waliyahiacademy@gmail.com>
2023-07-25 15:21:52 +0100

    first commit

---
+++ src/components/form.jsx
@@ -0,0 +1,93 @@
+import { useReducer, useState } from "react";
+import formReducer from "../reducers/formReducer";
+import { ACTIONS } from "../reducers/formReducer";
+
+const initialState = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  password: "",
+  showNextParts: false,
+  showAll: false,
+};
+
+const Form = () => {
+  const [state, dispatch] = useReducer(formReducer, initialState);
+  //   const [info, setInfo] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (state.firstName !== "" && state.lastName !== "") {
+      dispatch({ type: ACTIONS.SHOW_NEXT_PARTS });
+    } else {
+      alert("sup!");
+    }
+  };
+
+  const showAll = (e) => {
+    e.preventDefault();
+    if (state.showNextParts === true) {
+      dispatch({ type: ACTIONS.SHOW_ALL });
+    } else {
+      alert("hmmmmmmmm!!!");
+    }
+  };
+
+//   console.log(state);
+
+  function changeFIrstName(e) {
+    dispatch({ type: ACTIONS.ADD_FIRSTNAME, payload: e.target.value });
+  }
+  function changeLastName(e) {
+    dispatch({ type: ACTIONS.ADD_LASTNAME, payload: e.target.value });
+  }
+  function changeEmail(e) {
+    dispatch({ type: ACTIONS.ADD_EMAIL, payload: e.target.value });
+  }
+  function changePassword(e) {
+    dispatch({ type: ACTIONS.ADD_PASSWORD, payload: e.target.value });
+  }
+
+  return (
+    <>
+      <form>
+        <div style={{ marginBottom: "2em" }}>
+          <label htmlFor="firstName">first name</label>
+          <input
+            onInput={changeFIrstName}
+            type="text"
+            value={state.firstName}
+          />{" "}
+          <br />
+          <label htmlFor="firstName">first name</label>
+          <input onInput={changeLastName} type="text" value={state.lastName} />
+        </div>
+        <button onClick={handleSubmit}>next</button>
+        {state.showNextParts ? (
+          <div style={{ marginBottom: "2em" }}>
+            <label htmlFor="email">email</label>
+            <input onInput={changeEmail} type="text" value={state.email} />{" "}
+            <br />
+            <label htmlFor="password">password</label>
+            <input
+              onInput={changePassword}
+              type="text"
+              value={state.password}
+            />{" "}
+            <button onClick={showAll}>submit</button>
+          </div>
+        ) : null}
+        {state.showAll ? (
+          <div>
+            <p>{state.firstName}</p>
+            <p>{state.lastName}</p>
+            <p>{state.email}</p>
+            <p>{state.password}</p>
+          </div>
+        ) : null}
+      </form>
+    </>
+  );
+};
+
+export default Form;
---
+++ src/reducers/formReducer.js
@@ -0,0 +1,28 @@
+export const ACTIONS = {
+  ADD_FIRSTNAME: "add-firstName",
+  ADD_LASTNAME: "add-lastName",
+  ADD_EMAIL: "add-email",
+  ADD_PASSWORD: "add-password",
+  SHOW_NEXT_PARTS: "show-next",
+  SHOW_ALL: "show-all",
+};
+
+const formReducer = (state, action) => {
+  switch (action.type) {
+    case ACTIONS.ADD_FIRSTNAME:
+      return { ...state, firstName: action.payload };
+    case ACTIONS.ADD_LASTNAME:
+      return { ...state, lastName: action.payload };
+    case ACTIONS.ADD_EMAIL:
+      return { ...state, email: action.payload };
+    case ACTIONS.ADD_PASSWORD:
+      return { ...state, password: action.payload };
+    case ACTIONS.SHOW_NEXT_PARTS:
+      return { ...state, showNextParts: true};
+    case ACTIONS.SHOW_ALL:
+      return { ...state, showAll: true};
+  }
+  return state;
+};
+
+export default formReducer;
