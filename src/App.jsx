33b3e74004d147bfc8ccdc6d86e36fe21ddc32cd
Waliyah Okunade <waliyahiacademy@gmail.com>
2023-07-25 15:41:39 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import './App.css'
-import Form from './components/form'
+import "./App.css";
+import Form from "./components/form";
 
 function App() {
-
   return (
     <>
-      <Form/>
+      <Form />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
