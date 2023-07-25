72ecbe41cb9428a0292dcf75dc08cc3647a3f3e2
Waliyah Okunade <waliyahiacademy@gmail.com>
2023-07-25 15:41:17 +0100

    fourth commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-import { useState } from 'react'
 import './App.css'
 import Form from './components/form'
-import ConditionalForm from './components/shop'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
